ca92f4cb6a29d863ebc1ad47c105d078385bbfe3
SrutiGupta <mg6156710@gmail.com>
2025-10-24 12:48:37 +0530

    Logger setup Done

--- utils/logger.js
+++ utils/logger.js
@@ -6,10 +6,27 @@
 // ===============================================
 
 // 1️⃣ Import Winston library for advanced logging capabilities
+import winston from "winston";
 // 2️⃣ Configure log levels (error, warn, info, debug)
 // 3️⃣ Set up transports for console and file outputs
 // 4️⃣ Define log format with timestamps and log levels
+const formatlogger= winston.format.combine(
+    winston.format.timestamp({format :'YYYY-MM-DD hh mm ss'}),
+    winston.format.printf(({timestamps,level,message})=>{
+        return `$[timestamps] ${level.toUpperCase()} : ${message} `
+    })
+)
 // 5️⃣ Create and export logger instance for use throughout the application
+
+const logger = winston.createLogger({
+       level : "debug",
+       format :formatlogger,
+       transports :[
+        new winston.transport.console()
+       ]
+})
+
+export default logger;
 // 6️⃣ Provide functions for different log levels:
 //     - logger.info(message) for general information
 //     - logger.error(message) for error reporting
